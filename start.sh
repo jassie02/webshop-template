235b31845437c022815a7832d7593f654af6aad1
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2021-12-22 22:48:45 +0100

    remove my.db

--- start.sh
+++ start.sh
@@ -2,7 +2,10 @@ echo installing node.js packages according to package.json file
 echo this may take some minutes the first time
 yarn install
 
-echo genenating database from create.sql and saving to my.db
+echo genenating new database from create.sql and saving to my.db
+if [ -f db/my.db ]; then
+  rm db/my.db
+fi
 sqlite3 db/my.db < db/create.sql
 
 echo starting api and static webserver
