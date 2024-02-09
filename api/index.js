c20ae27ee4bc04b97377cc6e3a79d3e4ce542d12
jassie02 <158563949+jassie02@users.noreply.github.com>
2024-02-09 09:33:44 +0000

    volgorde

--- api/index.js
+++ api/index.js
@@ -57,7 +57,7 @@ function echoRequest(request, response) {
 function getCategories(request, response) {
   console.log('API ontvangt /api/categories/')
   // TODO: breid database uit zodat onderstaande query een lijstje categoriën levert.
-  const sqlOpdracht = db.prepare('SELECT categories.name AS category_name FROM categories ORDER BY id ASC')
+  const sqlOpdracht = db.prepare('SELECT categories.name AS category_name FROM categories ORDER BY id DESC')
   const data = sqlOpdracht.all()
   // console.log(JSON.stringify(data, null, 2))
   response.status(200).send(data)
